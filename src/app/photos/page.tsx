ee0016c515b56e40232ad1e4455318082b074d7a
Rock Gu <rock@subskribe.com>
2024-10-10 00:59:12 +0200

    better title

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -11,7 +11,7 @@ export default async function PhotoPage({
   const offset = parseInt(searchParams.offset as string) || 0;
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Show Me 10 Street Photos</h1>
+      <h1>Street Photos</h1>
       <StreetShowcase limit={limit} offset={offset} />
       <div className="container flex flex-row flex-wrap items-center justify-center gap-12 px-4 py-16"></div>
     </main>
