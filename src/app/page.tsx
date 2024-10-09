4f37440727968ab47e3e9ff75132042a74edc284
Rock Gu <rock@subskribe.com>
2024-10-09 12:14:39 +0200

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Pepe Images</h1>
+      {/* <h1>Pepe Images</h1> */}
       {/* <PepeShowcase pepes={pepes} /> */}
       <div className="container flex flex-row flex-wrap items-center justify-center gap-12 px-4 py-16"></div>
     </main>
